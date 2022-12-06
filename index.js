a33eb5aad3abe57f26d638a6d36df93ce24b0171
Benjamin Cook <bengcook55@gmail.com>
2022-12-05 21:03:54 -0500

    commit tree 3d53daf6378d503b6c1c94332ce3449cce435c97

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString = reverseLetters.join('')
+
+  if (word == reverseString){
+    return true 
+  } else {
+    return false 
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
+  declare arrayOfLetters to equal the argument passed into the parameter and splits the string into an array of substrings
+  declare anmother vartiable to take the new split array and reverse the letters from the initial argument
+  let another variable equal that variable joined which creates a new string by concatenating all elements from the newly made arrray when we split it.
+  create an if loop that suggests when the argument passed is equal to our split/reveresed/joined string, it will return as true. otherwise (else) it will return as false
+  */
+
 
 /*
   Add written explanation of your solution here
